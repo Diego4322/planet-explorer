0ea92047aa3af739bdfa1567d7a89241761962ff
Diego4322 <gldenpvp1@gmail.com>
2025-10-28 14:58:58 +0000

    started the superclass planet

---
+++ javaproject/src/com/myproject/Planet.java
@@ -0,0 +1,18 @@
+package com.myproject;
+//this is the superclass
+
+
+public class planet {
+    private String name;
+    private int size;
+    private int distance_from_sun;
+
+    public planet(String name, int size, int distance_from_sun) {
+        this.name = name;
+        this.size = size;
+        this.distance_from_sun = distance_from_sun;
+    }
+    
+
+    
+}
